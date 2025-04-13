1b97ca7773a4f4066b29b37a2c4f3f089ef5a854
Sambodhi <sambodhibhowal@Gmail.com>
2025-04-13 22:26:57 +0530

    frontline-lens init

--- front-end/src/app/pages/frontline-lens/frontline-lens.component.ts
+++ front-end/src/app/pages/frontline-lens/frontline-lens.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NgxFlickeringGridComponent } from '@omnedia/ngx-flickering-grid';
+import {NgxNeonUnderlineComponent} from "@omnedia/ngx-neon-underline"
 
 @Component({
   selector: 'app-frontline-lens',
-  imports: [],
+  imports: [NgxFlickeringGridComponent,  ],
   templateUrl: './frontline-lens.component.html',
   styleUrl: './frontline-lens.component.css'
 })
