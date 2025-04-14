3dee646d6465ef31b677d6cd2f5c71977c5bb1de
Sambodhi <sambodhibhowal@Gmail.com>
2025-04-14 13:30:38 +0530

    Footer , navbar updates

--- front-end/src/app/components/footer/footer.component.ts
+++ front-end/src/app/components/footer/footer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { NgxGridpatternComponent } from '@omnedia/ngx-gridpattern';
-
+import {NgxRetroGridComponent} from "@omnedia/ngx-retro-grid"
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [NgxGridpatternComponent],
+  imports: [NgxGridpatternComponent, NgxRetroGridComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
